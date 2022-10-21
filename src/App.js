ecd69751b1fb9fb5590c8cf01429f03eaadddc2a
Neil_Nguyen <higherblood2309@gmail.com>
2022-10-21 20:19:19 +0700

    day 1

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css"
+import Header from "./Components/common/Header/Header"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+
+function App() {
+  return (
+    <>
+    <Router>
+    <Header/>
+      
+    </Router>
+    </>
+  );
+}
+
+export default App;
